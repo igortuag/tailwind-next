e848729bdb6e81c229f5d69f2071370749b3a459
Tuag <igortuag@gmail.com>
2025-06-03 06:56:04 -0300

    feat: replace MainNavigation with NavItem components for improved navigation structure in Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,21 @@
-import { Search } from "lucide-react";
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users
+} from "lucide-react";
 import { Logo } from "./Logo";
 import { MainNavigation } from "./MainNavigation";
+import { NavItem } from "./NavItem";
 
 export const Sidebar = () => {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+    <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -15,7 +26,21 @@ export const Sidebar = () => {
         />
       </div>
 
-      <MainNavigation />
+      <nav>
+        <NavItem title="Home" icon={Home} href="/" />
+        <NavItem title="Dashboard" icon={BarChart} href="/dashboard" />
+        <NavItem title="Projects" icon={SquareStack} href="/projects" />
+        <NavItem title="Tasks" icon={CheckSquare} href="/tasks" />
+        <NavItem title="Reporting" icon={Flag} href="/reporting" />
+        <NavItem title="Users" icon={Users} href="/users" />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} href="/support" />
+          <NavItem title="Settings" icon={Cog} href="/settings" />
+        </nav>
+      </div>
     </aside>
   );
 };

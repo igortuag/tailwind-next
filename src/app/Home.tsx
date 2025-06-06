ec843b0fa5ea23df3796b578d052b33b2cef34c3
Tuag <igortuag@gmail.com>
2025-06-06 10:10:55 -0300

    feat: add SettingsTabs component for managing user settings

---
+++ src/app/Home.tsx
@@ -0,0 +1,11 @@
+import SettingsTabs from "@/components/SettingsTabs";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-medium">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  );
+}

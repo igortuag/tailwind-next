78fa3da84fdeeec822054af6f98ed4ec8664f351
Tuag <igortuag@gmail.com>
2025-06-07 06:53:28 -0300

    feat: implement TabItem component and integrate it into SettingsTabs for improved tab management

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,16 +1,26 @@
 "use client";
 import * as Tabs from "@radix-ui/react-tabs";
+import TabItem from "./TabItem";
+import { useState } from "react";
 
 export default function SettingsTabs() {
+  const [currentTab, setCurrentTab] = useState("my-details");
+
   return (
-    <Tabs.Root>
+    <Tabs.Root
+      onValueChange={setCurrentTab}
+    >
       <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        <Tabs.Trigger
-          className="px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
-          value="my-details"
-        >
-          <span>My details</span>
-        </Tabs.Trigger>
+        <TabItem value="my-details" title="My Details" isSelected={currentTab === "my-details"} />
+        <TabItem value="profile" title="Profile" isSelected={currentTab === "profile"} />
+        <TabItem value="password" title="Password" isSelected={currentTab === "password"} />
+        <TabItem value="team" title="Team" isSelected={currentTab === "team"} />
+        <TabItem value="plan" title="Plan" isSelected={currentTab === "plan"} />
+        <TabItem value="billing" title="Billing" isSelected={currentTab === "billing"} />
+        <TabItem value="email" title="Email" isSelected={currentTab === "email"} />
+        <TabItem value="notifications" title="Notifications" isSelected={currentTab === "notifications"} />
+        <TabItem value="integrations" title="Integrations" isSelected={currentTab === "integrations"} />
+        <TabItem value="api" title="API" isSelected={currentTab === "api"} />
       </Tabs.List>
     </Tabs.Root>
   );

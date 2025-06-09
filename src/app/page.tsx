ee037710f9255c14c53045359432e25e666fbdca
Tuag <igortuag@gmail.com>
2025-06-09 08:56:55 -0300

    feat: update Home component to display Settings header and integrate SettingsTabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import SettingsTabs from "@/components/SettingsTabs";
+
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-semibold">Hello Tailwind</h1>
+      <h1 className="text-2xl font-semibold">Settings</h1>
+
+      <SettingsTabs />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex justify-between items-center">
+          <div></div>
+          <div className="flex items-center gap-2">
+            <button type="button">Cancel</button>
+            <button type="submit" form="settings">Save</button>
+          </div>
+        </div>
+
+        <form id="settings-form" action=""></form>
+      </div>
     </div>
   );
 }

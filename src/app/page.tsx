7ad12250d22614553f2f8fce29a97574cf38610d
Tuag <igortuag@gmail.com>
2025-06-24 08:58:05 -0300

    feat: refactor file input components and integrate into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import SettingsTabs from "@/components/SettingsTabs";
 import * as Input from "@/components/Input";
+import * as FileInput from "@/components/Form/FileInput";
 import { Mail, UploadCloud, User } from "lucide-react";
 
 export default function Home() {
@@ -75,43 +76,11 @@ export default function Home() {
           </div>
 
           <div className="grid gap-3 grid-cols-form pt-5">
-            <label
-              htmlFor="photo"
-              className="text-sm font-medium text-zinc-700"
-            >
-              Your photo
-              <p className="text-sm font-normal text-zinc-500 mt-0.5">
-                This will be displayed on your profile.
-              </p>
-            </label>
-            <div className="flex items-start gap-5">
-              <div className="bg-violet-50 flex h-16 2-16 items-center justify-center rounded-full">
-                <User className="w-8 h-8 text-violet-500" />
-              </div>
-
-              <label
-                htmlFor="photo"
-                className="group flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
-              >
-                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
-                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
-                </div>
-
-                <div className="flex flex-col items-center gap-1">
-                  <span className="text-sm">
-                    <span className="font-semibold text-violet-700">
-                      Click to upload
-                    </span>
-                    or drag and drop
-                  </span>
-                  <span className="text-xs">
-                    SVG, PNG, JPG, or GIF (max, 800x400px)
-                  </span>
-                </div>
-              </label>
-
-              <input type="file" className="sr-only" id="photo" />
-            </div>
+            <FileInput.Root>
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
           </div>
 
           <div className="grid gap-3 grid-cols-form pt-5">
---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,4 @@
+export { Control } from "./Control";
+export { ImagePreview } from "./ImagePreview";
+export { Trigger } from "./Trigger";
+export { Root } from "./Root";

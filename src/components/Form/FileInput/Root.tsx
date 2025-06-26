7878c4fea84403ca02639d89aa4dc665b468dffd
Tuag <igortuag@gmail.com>
2025-06-26 09:08:59 -0300

    feat: add context provider to FileInput component for managing unique IDs

--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,7 +1,21 @@
-import { ComponentProps } from "react";
+import { ComponentProps, createContext, useId } from "react";
 
 export interface RootProps extends ComponentProps<"div"> {}
 
+type FileInputContextType = {
+  id: string;
+};
+
+export const FileInputContext = createContext<FileInputContextType | null>(
+  null
+);
+
 export function Root(props: RootProps) {
-  return <div {...props} />;
+  const id = useId();
+
+  return (
+    <FileInputContext.Provider value={{ id }}>
+      <div {...props} />
+    </FileInputContext.Provider>
+  );
 }

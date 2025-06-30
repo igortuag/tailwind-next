b3eb8b836454a9d8c3564c3d9ccf5eda3e2f9105
Tuag <igortuag@gmail.com>
2025-06-30 08:15:46 -0300

    feat: implement image preview functionality in ImagePreview component

--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,32 @@
 import { User } from "lucide-react";
+import { useFileInput } from "./Root";
+import { useMemo } from "react";
 
 export function ImagePreview() {
+  const { files } = useFileInput();
+
+  const previewUrl = useMemo(() => {
+    if (!files || files.length === 0) return null;
+
+    const objectUrl = URL.createObjectURL(files[0]);
+    return objectUrl;
+  }, [files]);
+
+  if (!previewUrl) {
+    return (
+      <div className="bg-violet-50 flex h-16 2-16 items-center justify-center rounded-full">
+        <User className="w-8 h-8 text-violet-500" />
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-violet-50 flex h-16 2-16 items-center justify-center rounded-full">
-      <User className="w-8 h-8 text-violet-500" />
-    </div>
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={previewUrl}
+      alt="Preview"
+      className="w-16 h-16 rounded-full object-cover"
+      onLoad={() => URL.revokeObjectURL(previewUrl)}
+    />
   );
 }

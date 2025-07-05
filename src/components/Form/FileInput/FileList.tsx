51025e31e1e166074f5f91a64e99a1a2c8d81157
Tuag <igortuag@gmail.com>
2025-07-05 06:43:01 -0300

    feat: add delete button to FileList component for file removal

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UploadCloud } from "lucide-react";
+import { Trash2, UploadCloud } from "lucide-react";
 import { useFileInput } from "./Root";
 import { formatBytes } from "@/app/ultils/format-bytes";
 
@@ -23,8 +23,25 @@ export function FileList() {
               <div className="text-sm font-medium text-zinc-700">
                 {file.name}
               </div>
-              <div className="text-sm text-zinc-500">{formatBytes(file.size)}</div>
+              <div className="text-sm text-zinc-500">
+                {formatBytes(file.size)}
+              </div>
             </div>
+
+            <div className="flex w-full items-center gap-3">
+              <div className="h-2 flex-1 rounded-full bg-zinc-100">
+                <div className="h-2 rounded-full bg-violet-600 w-4/5" />
+              </div>
+              <span className="text-sm font-medium text-zinc-500">80%</span>
+            </div>
+
+            <button
+              type="button"
+              className="ml-auto p-2 hover:bg-zinc-50 rounded-mds"
+            >
+              <Trash2 className="w-5 h-5 text-zinc-500" />
+              <span className="sr-only">Remove</span>
+            </button>
           </div>
         </div>
       ))}

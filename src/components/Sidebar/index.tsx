e789a6e57e6899747fcaa359f3e483a300b98c4e
Tuag <igortuag@gmail.com>
2025-08-16 07:37:35 -0300

    Refactor Sidebar to integrate Radix UI collapsible component and enhance layout structure

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   BarChart,
   CheckSquare,
@@ -5,6 +6,7 @@ import {
   Flag,
   Home,
   LifeBuoy,
+  Menu,
   Search,
   SquareStack,
   Users
@@ -13,41 +15,54 @@ import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import Profile from "./Profile";
+import * as Collapsable from "@radix-ui/react-collapsible";
 import * as Input from "../Input";
+import { Button } from "../Button";
 
 export const Sidebar = () => {
   return (
-    <aside className="border-b h-screen border-zinc-200 lg:px-5 p-4 lg:py-8 flex flex-col gap-6 fixed left-0 top-0 right-0 bottom-0 lg:bottom-auto z-20 bg-white lg:right-auto lg:w-80 lg:border-r">
-      <Logo />
+    <Collapsable.Root className="border-b h-screen border-zinc-200 lg:px-5 p-4 lg:py-8 flex flex-col gap-6 fixed left-0 top-0 right-0 bottom-0 lg:bottom-auto z-20 bg-white lg:right-auto lg:w-80 lg:border-r">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Button variant="ghost">
+          <Menu className="h-6 w-6" />
+          <Collapsable.Trigger className="lg:hidden"></Collapsable.Trigger>
+        </Button>
+      </div>
 
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="text-zinc-400" />
-        </Input.Prefix>
-        <Input.Control />
-      </Input.Root>
+      <Collapsable.Content
+        forceMount
+        className="flex flex-col gap-6 data-[state=close]:hidden lg:data-[state=close]:flex"
+      >
+        <Input.Root>
+          <Input.Prefix>
+            <Search className="text-zinc-400" />
+          </Input.Prefix>
+          <Input.Control />
+        </Input.Root>
 
-      <nav>
-        <NavItem title="Home" icon={Home} href="/" />
-        <NavItem title="Dashboard" icon={BarChart} href="/dashboard" />
-        <NavItem title="Projects" icon={SquareStack} href="/projects" />
-        <NavItem title="Tasks" icon={CheckSquare} href="/tasks" />
-        <NavItem title="Reporting" icon={Flag} href="/reporting" />
-        <NavItem title="Users" icon={Users} href="/users" />
-      </nav>
-
-      <div className="mt-auto flex flex-col gap-6">
-        <nav className="space-y-0.5">
-          <NavItem title="Support" icon={LifeBuoy} href="/support" />
-          <NavItem title="Settings" icon={Cog} href="/settings" />
+        <nav>
+          <NavItem title="Home" icon={Home} href="/" />
+          <NavItem title="Dashboard" icon={BarChart} href="/dashboard" />
+          <NavItem title="Projects" icon={SquareStack} href="/projects" />
+          <NavItem title="Tasks" icon={CheckSquare} href="/tasks" />
+          <NavItem title="Reporting" icon={Flag} href="/reporting" />
+          <NavItem title="Users" icon={Users} href="/users" />
         </nav>
 
-        <UsedSpaceWidget />
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem title="Support" icon={LifeBuoy} href="/support" />
+            <NavItem title="Settings" icon={Cog} href="/settings" />
+          </nav>
 
-        <div className="h-px bg-zinc-200" />
+          <UsedSpaceWidget />
 
-        <Profile />
-      </div>
-    </aside>
+          <div className="h-px bg-zinc-200" />
+
+          <Profile />
+        </div>
+      </Collapsable.Content>
+    </Collapsable.Root>
   );
 };

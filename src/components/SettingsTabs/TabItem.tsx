43f6ce150919c740635424a4d460c7cb071bdc2f
Tuag <igortuag@gmail.com>
2025-09-01 09:06:12 -0300

    Enhance dark mode styling for TabItem component

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -12,7 +12,7 @@ export interface TabItemProps {
 export default function TabItem({ value, title, isSelected }: TabItemProps) {
   return (
     <Tabs.Trigger
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700 outline-none group"
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 dark:text-zinc-400 hover:text-violet-700 dark:hover:text-violet-300 data-[state=active]:text-violet-700 dark:data-[state=active]:text-violet-300 outline-none group"
       value={value}
     >
       <span className="group-focus-visible:ring-2 rounded group-focus-visible:ring-violet-500 group-focus-visible:ring-offset-4 whitespace-nowrap">

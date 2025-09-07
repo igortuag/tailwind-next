9615dcf808a7091c5f90b318bde34cad03f8a738
Tuag <igortuag@gmail.com>
2025-09-07 06:39:41 -0300

    Enhance dark mode styling for Button and FileItem components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ const button = tv({
       primary:
         "bg-violet-600 text-white hover:bg-violet-700 dark:bg-violet-500 dark:hover:bg-violet-600",
       ghost:
-        "rounded-md px-2 hover:bg-zinc-50 dark:hover:bg-zinc-800 shadow-none text-zinc-500 dark:text-zinc-400",
+        "rounded-md px-2 hover:bg-zinc-50 dark:hover:bg-white/5 shadow-none text-zinc-500 dark:text-zinc-400",
       outline:
         "border border-zinc-300 text-zinc-700 hover:bg-zinc-50 dark:border-zin-c-700 dark:text-zinc-400 dark:hover:bg-zinc-800",
     },
